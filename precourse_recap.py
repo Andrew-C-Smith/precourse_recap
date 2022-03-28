8c05c36d9496b1ed63fdfd6a13df34d4efe0e6c6
Andrew Smith <andrew_c_smith@hotmail.com>
2022-03-28 16:31:17 +0100

    Second commit :)

---
+++ precourse_recap.py
@@ -0,0 +1,14 @@
+number_1 = 23
+number_2 = 34
+number_3 = 67
+
+print (number_1 * number_2)
+
+number_4 = (number_3 - number_2)
+
+print (number_4)
+print("Look at all these numbers" )
+
+name = ("andrew")
+second_name = ("smith")
+print (" My name is " + name + " " + second_name)
